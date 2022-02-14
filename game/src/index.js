4fd90084f3554d8313582c20a0ed5bcf5e40b22d
volha <v.khviadchenia@gmail.com>
2022-02-14 02:55:27 +0100

    feat: implement moving throw steps

--- game/src/index.js
+++ game/src/index.js
@@ -53,12 +53,20 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     }
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -69,14 +77,27 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const movies = history.map((step, movie) => {
+      const desc = movie ? 
+        'Переход к ходу #' + movie :
+        'К началу игры';
+      return (
+        <li key={movie}>
+          <button onClick={() => this.jumpTo(movie)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = 'Выиграл ' + winner;
@@ -94,7 +115,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div className='status'>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{movies}</ol>
         </div>
       </div>
     );
